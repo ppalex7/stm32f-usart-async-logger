8a2e04c66066c8578a29fc68153292ac2a62981f
Aleksandr Duplishchev <2ppalex@gmail.com>
2025-09-21 15:21:05 +0300

    add empty arg as default

--- uart_logger.hpp
+++ uart_logger.hpp
@@ -1,7 +1,7 @@
 #ifndef __UART_LOGGER_HPP
 #define __UART_LOGGER_HPP
 
-void uart_log(const char *const fmt, uint16_t arg);
+void uart_log(const char *const fmt, uint16_t arg = 0);
 void configure_logger_peripheral(const uint32_t usart_brr);
 void on_dma_log_transfer_complete();
 void process_buffered_logs();
